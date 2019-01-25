f834b1561e84be6218e79d1c93a750cc231bd0b0
Ryan Levick <ryan.levick@gmail.com>
2019-01-25 13:59:15 +0100

    Update hyper

--- src/main.rs
+++ src/main.rs
@@ -19,22 +19,15 @@ async I/O, by futures running in a thread pool (using [futures_cpupool]).
 extern crate error_type;
 
 use clap::App;
-use futures::{Async, Future, Poll};
-use futures_cpupool::{CpuFuture, CpuPool};
-use hyper::{
-    header::{ContentLength, ContentType},
-    mime,
-    server::{Http, Request, Response, Service},
-    StatusCode,
-};
+use futures::{future::Either, Future};
+use http::status::StatusCode;
+use hyper::{header, service::service_fn, Body, Request, Response, Server};
 use std::{
     error::Error as StdError,
-    fs::File,
-    io::{self, Read},
+    io,
     net::SocketAddr,
     path::{Path, PathBuf},
 };
-
 fn main() {
     // Set up our error handling immediatly. Everything in this crate
     // that can return an error returns our custom Error type. `?`
@@ -49,28 +42,23 @@ fn main() {
 
 fn run() -> Result<(), Error> {
     // Create the configuration from the command line arguments. It
-    // includes the IP address and port to listen on, the path to use
-    // as the HTTP server's root directory, and the file I/O thread
-    // pool.
+    // includes the IP address and port to listen on and the path to use
+    // as the HTTP server's root directory
     let config = parse_config_from_cmdline()?;
-    let Config {
-        addr,
-        root_dir,
-        num_file_threads,
-        ..
-    } = config;
+    let Config { addr, root_dir, .. } = config;
 
-    // Create HTTP service, passing the document root directory and the
-    // thread pool used for executing the file reading I/O on.
-    let server = Http::new()
-        .bind(&addr, move || {
-            Ok(HttpService {
-                root_dir: root_dir.clone(),
-                pool: CpuPool::new(num_file_threads),
-            })
+    // Create HTTP service, passing the document root directory
+    let server = Server::bind(&addr)
+        .serve(move || {
+            let root_dir = root_dir.clone();
+            service_fn(move |req| serve(root_dir.clone(), req))
         })
-        .unwrap();
-    server.run().unwrap();
+        .map_err(|e| {
+            println!("There was an error: {}", e);
+            ()
+        });
+
+    hyper::rt::run(server);
     Ok(())
 }
 
@@ -79,8 +67,6 @@ fn run() -> Result<(), Error> {
 struct Config {
     addr: SocketAddr,
     root_dir: PathBuf,
-    num_file_threads: usize,
-    num_server_threads: u16,
 }
 
 fn parse_config_from_cmdline() -> Result<Config, Error> {
@@ -89,117 +75,62 @@ fn parse_config_from_cmdline() -> Result<Config, Error> {
         .about("A basic HTTP file server")
         .args_from_usage(
             "[ROOT] 'Sets the root dir (default \".\")'
-             [ADDR] -a --addr=[ADDR] 'Sets the IP:PORT combination (default \"127.0.0.1:4000\")'
-             [THREADS] -t --threads=[THREADS] 'Sets the number of server threads (default 4)'
-             [FILE-THREADS] --file-threads=[FILE-THREADS] 'Sets the number of threads in the file I/O thread pool (default 100)'")
+             [ADDR] -a --addr=[ADDR] 'Sets the IP:PORT combination (default \"127.0.0.1:4000\")'",
+        )
         .get_matches();
-
-    let default_server_threads = 4;
-    let default_file_threads = 100;
 
     let addr = matches.value_of("ADDR").unwrap_or("127.0.0.1:4000");
     let root_dir = matches.value_of("ROOT").unwrap_or(".");
-    let num_server_threads = match matches.value_of("THREADS") {
-        Some(t) => t.parse()?,
-        None => default_server_threads,
-    };
-    let num_file_threads = match matches.value_of("FILE-THREADS") {
-        Some(t) => t.parse()?,
-        None => default_file_threads,
-    };
 
     // Display the configuration to be helpful
     println!("addr: http://{}", addr);
     println!("root dir: {:?}", root_dir);
-    println!("server threads: {}", num_server_threads);
-    println!("file threads: {}", num_file_threads);
     println!("");
 
     Ok(Config {
         addr: addr.parse()?,
         root_dir: PathBuf::from(root_dir),
-        num_file_threads: num_file_threads,
-        num_server_threads: num_server_threads,
     })
 }
 
-struct HttpService {
+fn serve(
     root_dir: PathBuf,
-    pool: CpuPool,
-}
-
-// The HttpService knows how to build a ResponseFuture for each hyper Request
-// that is received. Errors are turned into an Error response (404 or 500).
-impl Service for HttpService {
-    type Request = Request;
-    type Response = Response;
-    type Error = hyper::Error;
-    type Future = ResponseFuture;
-    fn call(&self, req: Request) -> Self::Future {
-        let uri_path = req.uri().path();
-        if let Some(path) = local_path_for_request(&uri_path, &self.root_dir) {
-            ResponseFuture::File(self.pool.spawn(FileFuture { path }))
-        } else {
-            ResponseFuture::Error
-        }
-    }
-}
-
-enum ResponseFuture {
-    File(CpuFuture<Response, Error>),
-    Error,
-}
-
-impl Future for ResponseFuture {
-    type Item = Response;
-    type Error = hyper::Error;
-    fn poll(&mut self) -> Poll<Response, hyper::Error> {
-        match *self {
-            // If this is a File variant, poll the contained CpuFuture
-            // and propagate the result outward as a Response.
-            ResponseFuture::File(ref mut f) => match f.poll() {
-                Ok(Async::Ready(rsp)) => Ok(Async::Ready(rsp)),
-                Ok(Async::NotReady) => Ok(Async::NotReady),
-                Err(_) => Ok(Async::Ready(internal_server_error())),
-            },
-            // For the Error variant, we can just return an error immediately.
-            ResponseFuture::Error => Ok(Async::Ready(internal_server_error())),
-        }
-    }
-}
-
-struct FileFuture {
-    path: PathBuf,
-}
-
-impl Future for FileFuture {
-    type Item = Response;
-    type Error = Error;
-    fn poll(&mut self) -> Poll<Response, Error> {
-        match File::open(&self.path) {
-            Ok(mut file) => {
-                let mut buf = Vec::new();
-                match file.read_to_end(&mut buf) {
-                    Ok(_) => {
-                        let mime_type = file_path_mime(&self.path);
-                        Ok(Async::Ready(
-                            Response::new()
-                                .with_status(StatusCode::Ok)
-                                .with_header(ContentLength(buf.len() as u64))
-                                .with_header(ContentType(mime_type))
-                                .with_body(buf),
-                        ))
-                    }
-                    Err(_) => Ok(Async::Ready(internal_server_error())),
+    req: Request<Body>,
+) -> impl Future<Item = Response<Body>, Error = Error> {
+    let uri_path = req.uri().path();
+    if let Some(path) = local_path_for_request(&uri_path, &root_dir) {
+        let future =
+            tokio_fs::file::File::open(path.clone()).then(|open_result| match open_result {
+                Ok(file) => {
+                    let buf: Vec<u8> = Vec::new();
+                    let future = tokio_io::io::read_to_end(file, buf)
+                        .map_err(Error::Io)
+                        .and_then(move |(_, buf)| {
+                            let mime_type = file_path_mime(&path);
+                            Response::builder()
+                                .status(StatusCode::OK)
+                                .header(header::CONTENT_LENGTH, buf.len() as u64)
+                                .header(header::CONTENT_TYPE, mime_type.as_ref())
+                                .body(Body::from(buf))
+                                .map_err(Error::from)
+                        });
+                    Either::A(future)
                 }
-            }
-            Err(e) => match e.kind() {
-                io::ErrorKind::NotFound => Ok(Async::Ready(
-                    Response::new().with_status(StatusCode::NotFound),
-                )),
-                _ => Ok(Async::Ready(internal_server_error())),
-            },
-        }
+                Err(e) => {
+                    let future = match e.kind() {
+                        io::ErrorKind::NotFound => Either::A(futures::future::result(
+                            Response::builder()
+                                .status(StatusCode::NOT_FOUND)
+                                .body(Body::empty()),
+                        )),
+                        _ => Either::B(internal_server_error()),
+                    };
+                    Either::B(future.map_err(Error::from))
+                }
+            });
+        Either::A(future)
+    } else {
+        Either::B(internal_server_error().map_err(Error::from))
     }
 }
 
@@ -240,10 +171,13 @@ fn local_path_for_request(request_path: &str, root_dir: &Path) -> Option<PathBuf
     Some(path)
 }
 
-fn internal_server_error() -> Response {
-    Response::new()
-        .with_status(StatusCode::InternalServerError)
-        .with_header(ContentLength(0))
+fn internal_server_error() -> impl Future<Item = Response<Body>, Error = http::Error> {
+    futures::future::result(
+        Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .header(header::CONTENT_LENGTH, 0)
+            .body(Body::empty()),
+    )
 }
 
 // The custom Error type that encapsulates all the possible errors
@@ -254,6 +188,7 @@ error_type! {
     #[derive(Debug)]
     enum Error {
         Io(io::Error) { },
+        HttpError(http::Error) { },
         AddrParse(std::net::AddrParseError) { },
         Std(Box<StdError + Send + Sync>) {
             desc (e) e.description();
